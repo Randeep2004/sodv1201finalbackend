a72bb8bcad9c23175f5494811ae5f83913736bdf
Randeep2004 <149605904+Randeep2004@users.noreply.github.com>
2024-04-25 23:26:52 -0600

    last update

--- connectdb.js
+++ connectdb.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose')
-const MongoDB_URI = "mongodb+srv://user:<password>@signup.3jrxzia.mongodb.net/?retryWrites=true&w=majority&appName=signupnode"
+const MongoDB_URI = "mongodb+srv://user:user@signup.3jrxzia.mongodb.net/?retryWrites=true&w=majority&appName=signup"
 async function connectToMongoDB() {
     try {
         await mongoose.connect(MongoDB_URI);
         console.log("Connected to MongoDB");
     } catch (err) {
-
         console.error("Error connecting to MongoDB:", err)
-
     }
 }
 
